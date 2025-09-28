b2817abcd5c256b89cebf53ca3252b819ab2e513
Nemuichi <ramondiasperes04@gmail.com>
2025-09-28 20:05:52 -0300

    programa feito

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿double media, num1, num2, num3, num4;
+ 
+Console.WriteLine("----------Qual a média da sua nota bimestral?----------");
+
+Console.Write("Digite sua primeira nota: ");
+ num1 = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Digite sua segunda nota: ");
+ num2 = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Digite sua terceira nota: ");
+ num3 = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Digite sua quarta nota: ");
+ num4 = Convert.ToDouble(Console.ReadLine()!);
+
+media = (num1 + num2 + num3 + num4) / 4;    
+
+Console.WriteLine($"Sua média anual é: {media.ToString("N1")}");
